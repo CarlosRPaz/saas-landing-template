bab555d8c8ba85e61d1e4f9589e7b467ceb8fd18
CarlosRPaz <trojan.carlos44@gmail.com>
2022-02-19 14:51:11 -0800

    Applied styles to globals.css, renamed original Header to Navbar, created new Header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
+import Navbar from '../components/Navbar'
 import Header from '../components/Header'
-
 
 export default function Home() {
   return (
@@ -10,8 +10,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <Navbar />
       <Header />
-
 
     </div>
   )
